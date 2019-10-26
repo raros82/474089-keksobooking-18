9e2b5b169f1a999222bf4ab0973a114461254f89
Roman Rateev <raros82@gmail.com>
2019-10-26 15:36:13 +0300

    Внес корректировки наставника

--- js/main.js
+++ js/main.js
@@ -11,37 +11,31 @@ var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditio
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var OFFERS_QUANTITY = 8;
 
-var getRandomArrElements = function (arr) {
-  var randomArrElements = [];
-  var randomQuantityOfArrElements = getRandomInteger(0, arr.length);
-  for (var i = 0; i < randomQuantityOfArrElements; i++) {
-    randomArrElements[i] = arr[i];
-  }
-  return randomArrElements;
-};
 
 var generateRentalOfferMocks = function (quantity) {
   var rentalOffersMocks = [];
   for (var i = 1; i <= quantity; i++) {
     var rentalOffer = {};
-    rentalOffer.author = {avatar: 'img/avatars/user0' + [i] + '.png'};
+    rentalOffer.author = {avatar: 'img/avatars/user0' + i + '.png'};
+    rentalOffer.location = {
+      x: getRandomInteger(0, 1200),
+      y: getRandomInteger(130, 630)
+    };
     rentalOffer.offer = {
-      title: 'Заголовок предложения ' + [i],
-      address: getRandomInteger(0, 600) + ', ' + getRandomInteger(0, 600),
+      title: 'Заголовок предложения ' + i,
+      // address: getRandomInteger(0, 1200) + ', ' + getRandomInteger(130, 630),
+      address: rentalOffer.location.x + ', ' + rentalOffer.location.y,
       price: getRandomInteger(100, 500),
       type: TYPE[getRandomInteger(0, 3)],
       rooms: getRandomInteger(1, 5),
       guests: getRandomInteger(1, 5),
       checkin: CHECKIN[getRandomInteger(0, 2)],
       checkout: CHECKOUT[getRandomInteger(0, 2)],
-      features: getRandomArrElements(FEATURES),
-      description: 'Описание ' + [i],
-      photos: getRandomArrElements(PHOTOS)
+      features: FEATURES.slice(0, getRandomInteger(1, FEATURES.length)),
+      description: 'Описание ' + i,
+      photos: PHOTOS.slice(0, getRandomInteger(1, PHOTOS.length))
     };
-    rentalOffer.location = {
-      x: getRandomInteger(0, 1200),
-      y: getRandomInteger(130, 630)
-    };
+
     rentalOffersMocks.push(rentalOffer);
   }
   return rentalOffersMocks;
