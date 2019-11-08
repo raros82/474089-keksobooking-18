b3615b0f89e21fea268c0ead5f1bae5e7a9f9009
Roman Rateev <raros82@gmail.com>
2019-11-08 11:34:46 +0200

    Внес корректировки от npm test - fake

--- js/main.js
+++ js/main.js
@@ -1,66 +1,66 @@
 'use strict';
 
-var getRandomInteger = function (min, max) {
-  return Math.floor(min + Math.random() * (max - min + 1));
-};
+// var getRandomInteger = function (min, max) {
+//   return Math.floor(min + Math.random() * (max - min + 1));
+// };
 
-var TYPE = ['palace', 'flat', 'house', 'bungalo'];
-var CHECKIN = ['12:00', '13:00', '14:00'];
-var CHECKOUT = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var OFFERS_QUANTITY = 8;
+// var TYPE = ['palace', 'flat', 'house', 'bungalo'];
+// var CHECKIN = ['12:00', '13:00', '14:00'];
+// var CHECKOUT = ['12:00', '13:00', '14:00'];
+// var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+// var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+// var OFFERS_QUANTITY = 8;
 
 var ENTER_KEYCODE = 13;
 
-var PIN_ARROW_HEIGHT = 22;
+// var PIN_ARROW_HEIGHT = 22;
 
-var generateRentalOfferMocks = function (quantity) {
-  var rentalOffersMocks = [];
-  for (var i = 1; i <= quantity; i++) {
-    var rentalOffer = {};
-    rentalOffer.author = {avatar: 'img/avatars/user0' + i + '.png'};
-    rentalOffer.location = {
-      x: getRandomInteger(0, 1200),
-      y: getRandomInteger(130, 630)
-    };
-    rentalOffer.offer = {
-      title: 'Заголовок предложения ' + i,
-      address: rentalOffer.location.x + ', ' + rentalOffer.location.y,
-      price: getRandomInteger(100, 500),
-      type: TYPE[getRandomInteger(0, 3)],
-      rooms: getRandomInteger(1, 5),
-      guests: getRandomInteger(1, 5),
-      checkin: CHECKIN[getRandomInteger(0, 2)],
-      checkout: CHECKOUT[getRandomInteger(0, 2)],
-      features: FEATURES.slice(0, getRandomInteger(1, FEATURES.length)),
-      description: 'Описание ' + i,
-      photos: PHOTOS.slice(0, getRandomInteger(1, PHOTOS.length))
-    };
+// var generateRentalOfferMocks = function (quantity) {
+//   var rentalOffersMocks = [];
+//   for (var i = 1; i <= quantity; i++) {
+//     var rentalOffer = {};
+//     rentalOffer.author = {avatar: 'img/avatars/user0' + i + '.png'};
+//     rentalOffer.location = {
+//       x: getRandomInteger(0, 1200),
+//       y: getRandomInteger(130, 630)
+//     };
+//     rentalOffer.offer = {
+//       title: 'Заголовок предложения ' + i,
+//       address: rentalOffer.location.x + ', ' + rentalOffer.location.y,
+//       price: getRandomInteger(100, 500),
+//       type: TYPE[getRandomInteger(0, 3)],
+//       rooms: getRandomInteger(1, 5),
+//       guests: getRandomInteger(1, 5),
+//       checkin: CHECKIN[getRandomInteger(0, 2)],
+//       checkout: CHECKOUT[getRandomInteger(0, 2)],
+//       features: FEATURES.slice(0, getRandomInteger(1, FEATURES.length)),
+//       description: 'Описание ' + i,
+//       photos: PHOTOS.slice(0, getRandomInteger(1, PHOTOS.length))
+//     };
+//
+//     rentalOffersMocks.push(rentalOffer);
+//   }
+//   return rentalOffersMocks;
+// };
 
-    rentalOffersMocks.push(rentalOffer);
-  }
-  return rentalOffersMocks;
-};
-
-var mocks = generateRentalOfferMocks(OFFERS_QUANTITY);
+// var mocks = generateRentalOfferMocks(OFFERS_QUANTITY);
 
 var map = document.querySelector('.map');
 // map.classList.remove('map--faded');
 
-var mapPins = document.querySelector('.map__pins');
-var mapPinTemplate = document.querySelector('#pin')
-  .content
-  .querySelector('.map__pin');
+// var mapPins = document.querySelector('.map__pins');
+// var mapPinTemplate = document.querySelector('#pin')
+//   .content
+//   .querySelector('.map__pin');
 
 
-var renderRentalOffer = function (offerObj) {
-  var offer = mapPinTemplate.cloneNode(true);
-  offer.style = 'left: ' + (offerObj.location.x - 25) + 'px; top: ' + (offerObj.location.y - 70) + 'px;';
-  offer.querySelector('img').src = offerObj.author.avatar;
-  offer.querySelector('img').alt = offerObj.offer.title;
-  return offer;
-};
+// var renderRentalOffer = function (offerObj) {
+//   var offer = mapPinTemplate.cloneNode(true);
+//   offer.style = 'left: ' + (offerObj.location.x - 25) + 'px; top: ' + (offerObj.location.y - 70) + 'px;';
+//   offer.querySelector('img').src = offerObj.author.avatar;
+//   offer.querySelector('img').alt = offerObj.offer.title;
+//   return offer;
+// };
 
 // var fragment = document.createDocumentFragment();
 // for (var i = 0; i < mocks.length; i++) {
@@ -112,7 +112,7 @@ var mapPinMain = document.querySelector('.map__pin--main');
 var turnPageToActiveState = function () {
   map.classList.remove('map--faded');
   mapFilters.classList.remove('map__filters--disabled');
-  for (var i = 0; i < adFormFieldset.length; i++) {
+  for (i = 0; i < adFormFieldset.length; i++) {
     adFormFieldset[i].removeAttribute('disabled');
   }
   adForm.classList.remove('ad-form--disabled');
